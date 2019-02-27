a3b5134336a383d331cd664355549208ae0678c2
Asama <asamahayder@yahoo.dk>
2019-02-27 18:19:55 +0100

    gjorde tingeling

--- src/dal/UserDAO_al.java
+++ src/dal/UserDAO_al.java
@@ -1,4 +1,70 @@
 package dal;
+import dto.User;
 
-public class UserDAO_al {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UserDAO_al implements IUserDAO {
+
+    private List<User> userList = new ArrayList<>();
+
+    @Override
+    public User getUser(int userId) {
+        userId-=1;
+        User user = new User(0);
+
+        user.setUserId(user.getUserId());
+        user.setUserName(user.getUserName());
+        user.setIni(user.getIni());
+        user.setRoles(user.getRoles());
+        user.setCpr(user.getCpr());
+        user.setPassword(user.getPassword());
+        return user;
+    }
+
+    @Override
+    public List<User> getUserList() {
+        ArrayList<User> userArrayList = new ArrayList<>();
+
+        for (int i = 0; i < userArrayList.size(); i++) {
+            User user = new User(0);
+            userArrayList.add(user);
+
+        }
+
+        /*for (int i = 0; i < userList.size(); i++){
+            System.out.println(userList.get(i));
+        }*/
+
+        return userList;
+    }
+
+    @Override
+    public void createUser(User user)  {
+        userList.add(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+        userList.get(1).setRoles(Collections.singletonList("asd"));
+    }
+
+    @Override
+    public void deleteUser(int userId) {
+
+        userList.remove(userId-1);
+    }
+
+    @Override
+    public void createConnection() throws SQLException {
+
+    }
+
+    @Override
+    public void closeConnection() throws SQLException {
+
+    }
 }
